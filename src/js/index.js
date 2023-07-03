3385b2cd4babbd4dd2390ec8e635fbe4e3cc828f
Aline Serafim <aline.serafim86@gmail.comgit config --global user.name Aline>
2023-07-03 15:07:02 -0300

    retificando o projeto

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,3 @@
-// const inputs = document.querySelectorAll(".inputs");
-// const contactBtn = document.querySelector(".contact-btn");
-
-// contactBtn.addEventListener("click", (event) => {
-//     event.preventDefault();
-
-//     inputs.forEach((input) => {
-//         const errorMsg = input.nextElementSibling;
-
-//         if (input.value === "") {
-//             input.classList.add("error-msg");
-//             input.classList.remove("form-confirmed");
-//             errorMsg.textContent = "campo obrigatório";
-//             errorMsg.classList.add("span-required");
-//         } else {
-//             errorMsg.textContent = "";
-//             errorMsg.classList.remove("span-required");
-//             input.classList.remove("error-msg");
-//             input.classList.add("form-confirmed");
-//         }
-//     });
-// });
-
 const camposFormulario = document.querySelectorAll('.campo')
 const botaoEnviar = document.querySelector('.btn-enviar')
 
